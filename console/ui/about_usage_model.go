ddf7c4b18ec8b2e526a256c84a840aae80abf33f
phil.zhang <phil.zhang@imotion.ai>
2024-11-04 15:30:28 +0800

    optimize syles for usage

--- console/ui/about_usage_model.go
+++ console/ui/about_usage_model.go
@@ -10,17 +10,13 @@ import (
 func createUsageModel(goback func()) usageModel {
 	toolchainPath, _ := filepath.Abs("cmake/buildenv.cmake")
 
-	content := fmt.Sprintf(`
-Welcome to the buildenv.
------------------------------------
-
-This is a simple tool to manage your cross build environment.
-
-Usages:
-option1: set(CMAKE_TOOLCHAIN_FILE "%s")
-option2: cmake .. -DCMAKE_TOOLCHAIN_FILE %s
-
-[press ctrl+c or q to quit]`, toolchainPath, toolchainPath)
+	content := fmt.Sprintf("\nWelcome to buildenv.\n"+
+		"-----------------------------------\n"+
+		"This is a simple tool to manage your cross build environment.\n\n"+
+		"How to use in cmake project: \n"+
+		"\033[34moption1: set(CMAKE_TOOLCHAIN_FILE \"%s\")\033[0m\n"+
+		"\033[34moption2: cmake .. -DCMAKE_TOOLCHAIN_FILE %s\033[0m\n\n"+
+		"[press ctrl+c or q to quit]", toolchainPath, toolchainPath)
 	return usageModel{content: content, goback: goback}
 }
 
