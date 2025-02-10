d084dff5d77a25204d7fb383004e60ec5a83f08b
phil.zhang <phil.zhang@imotion.ai>
2025-02-10 22:38:44 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ export GOPROXY=https://goproxy.io,direct
 VERSION=v0.1.0
 go build -trimpath -ldflags "-s -w -X buildenv/cmd/cli.Version=${VERSION}"
 
-if [ -x "upx" ]; then
-    upx --best buildenv
+upx=$(which upx)
+
+if [ -x "$upx" ]; then
+    "$upx" --best buildenv
 fi
